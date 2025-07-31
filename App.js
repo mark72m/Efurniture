50a64c988816ea806be0a1f4ef52b0ec47f526ff
markm <markmutisya72@gmail.com>
2025-07-31 04:43:53 -0700

    App js Edit

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
 export default function App() {
-  return (
+  const [fontsLoaded] =  useFonts({
+    regular: require('./assets/fonts/Poppins-Regular.ttf'),
+    light: require('./assets/fonts/Poppins-Light.ttf'),
+    bold: require('./assets/fonts/Poppins-Bold.ttf'),
+    medium: require('./assets/fonts/Poppins-Medium.ttf'),
+    extrabold: require('./assets/fonts/Poppins-ExtraBold.ttf'),
+    semibold: require('./assets/fonts/Poppins-SemiBold.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded) {
+    return null;
+  }
+   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Op up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontFamily: 'semibold',
+    fontSize: 20
+  }
 });
