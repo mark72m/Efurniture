4dbdea59c24a10a02e17df1828d5c2ab5d6f5c87
markm <markmutisya72@gmail.com>
2025-08-01 07:07:15 -0700

    Updates Carousel

--- components/home/Welcome.jsx
+++ components/home/Welcome.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 import styles from './welcome.styles'
 import { COLORS, SIZES } from '../../constants'
 import { Feather, Ionicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 const Welcome = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <View style={styles.container}>
@@ -21,9 +23,10 @@ const Welcome = () => {
           <TextInput
             style={styles.searchInput}
             value=''
-            onPressIn={() => { }}
+            onPressIn={() => navigation.navigate('Search')}
             placeholder="What are you looking for" />
         </View>
+
         <View>
           <TouchableOpacity style={styles.searchBtn}>
             <Ionicons name='camera-outline' size={SIZES.xLarge} color={COLORS.offwhite} />
--- screens/search.style.js
+++ screens/search.style.js
@@ -1,18 +1,8 @@
-import { StyleSheet } from 'react-native';
-import { COLORS, SIZES } from "../../constants/index";
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../constants";
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%'
-    },
-    welcomeTxt: (color, top) => ({
-        fontFamily: "bold",
-        fontSize: SIZES.xxLarge - 5,
-        marginTop: top,
-        color: color,
-        marginHorizontal: SIZES.small
-    }),
-    searchContainer: {
+searchContainer: {
         flexDirection: 'row',
         justifyContent: "center",
         alignContent: 'center',
