cf14e76771b054a8a5295ea76264d21de9076302
markm <markmutisya72@gmail.com>
2025-08-02 11:15:45 -0700

    Product Card View

--- components/products/ProductCardView.jsx
+++ components/products/ProductCardView.jsx
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import styles from './productCardView.style'
 
 const ProductCardView = () => {
   return (
-    <View>
-      <Text>ProductCardView</Text>
-    </View>
+    <TouchableOpacity onPress={() => {}}>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
+                source={{uri: "https://d326fntlu7tb1e.cloudfront.net/uploads/cb2e64a8-ad4c-4d45-b58b-b0c7e11b6bb4-fn1.jpg"}} 
+                style={styles.image}/>
+
+            </View>
+        </View>
+    </TouchableOpacity>
   )
 }
 
--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -1,6 +1,7 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SIZES } from '../../constants'
+import ProductCardView from './ProductCardView'
 
 const ProductRow = () => {
     const products = [1, 2, 3, 4]
@@ -8,7 +9,7 @@ const ProductRow = () => {
         <View>
             <FlatList
                 data={products}
-                renderItem={({ item }) => (<Text>Products</Text>)}
+                renderItem={({ item }) => (<ProductCardView/>)}
                 horizontal
                 contentContainerStyle={{ columnGap: SIZES.medium }} />
         </View>
