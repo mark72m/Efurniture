3f07e3ce2400b1c4df1fb4db00dfc777db4a9c0b
markm <markmutisya72@gmail.com>
2025-08-03 04:44:48 -0700

    Updates

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, Image, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import styles from './productDetails.style'
 import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '../constants';
 
 const ProductDetails = ({navigation}) => {
   return (
@@ -10,6 +11,37 @@ const ProductDetails = ({navigation}) => {
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <Ionicons name='chevron-back-circle' size={30} />
         </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {}}>
+          <Ionicons name='heart' size={30} color={COLORS.primary}/>
+        </TouchableOpacity>
+      </View>
+      <Image 
+      source={{uri: "https://cottage-culture.ca/userContent/images/E-Commerce/Collections/Pamir/84253%20Callisto_lounge_Virgo_Tonar_Pamir_4%20web_84253%20DS.jpg"}}
+      style={styles.image}/>
+      <View style={styles.details}>
+        <View style={styles.titleRow}>
+          <Text style={styles.title}>Product</Text>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>$25.50</Text>
+          </View>
+        </View>
+
+        <View style={styles.ratingRow}>
+          <View style={styles.rating}>
+            {[1, 2, 3, 4, 5].map((index) => (
+              <Ionicons
+              key={index}
+              name='star'
+              size={24}
+              color="gold" />
+            ))}
+
+            <Text style={styles.ratingText}> (4.9) </Text>
+            
+          </View>
+        </View>
+
       </View>
     </View>
   )
