959ff134ec0f3f2be2e4b7843108ce4eb082a8ca
markm <markmutisya72@gmail.com>
2025-08-03 03:53:05 -0700

    Continuation

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -1,12 +1,13 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import React from 'react'
 import { SIZES } from '../../constants'
 import ProductCardView from './ProductCardView'
+import styles from './productRow.style'
 
 const ProductRow = () => {
     const products = [1, 2, 3, 4]
     return (
-        <View>
+        <View style={styles.container}>
             <FlatList
                 data={products}
                 renderItem={({ item }) => (<ProductCardView/>)}
@@ -17,5 +18,3 @@ const ProductRow = () => {
 }
 
 export default ProductRow
-
-const styles = StyleSheet.create({})
