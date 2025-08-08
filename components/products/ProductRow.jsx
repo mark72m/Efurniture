777f50a3c8c7b306b8330c2e862fbdb6de9e83a2
markm <markmutisya72@gmail.com>
2025-08-07 23:48:38 -0700

    Error Correction

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 import { COLORS, SIZES } from '../../constants'
 import ProductCardView from './ProductCardView'
 import styles from './productRow.style'
-import useFetch  from '../../hook/useFetch'
+import useFetch from '../../hook/useFetch'
 
 const ProductRow = () => {
-    const {data, isLoading, error} = useFetch();
+    const { data, isLoading, error } = useFetch();
     return (
         <View style={styles.container}>
             {isLoading ? (
-                <ActivityIndicator size={SIZES.xxLarge} color={COLORS.primary}/>
-            ): error ? (
+                <ActivityIndicator size={SIZES.xxLarge} color={COLORS.primary} />
+            ) : error ? (
                 <Text>Something went wrong!!!</Text>
-            ): (
+            ) : (
                 <FlatList
-                data={data}
-                keyExtractor={(item) => item._id}
-                renderItem={({ item }) => <ProductCardView item = {item}/>}
-                horizontal
-                contentContainerStyle={{ columnGap: SIZES.medium }} />
-            )}            
+                    data={data}
+                    keyExtractor={(item) => item._id}
+                    renderItem={({ item }) => <ProductCardView item={item} />}
+                    horizontal
+                    contentContainerStyle={{ columnGap: SIZES.medium }} />
+            )}
         </View>
     )
 }
